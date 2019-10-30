9f5356856b7f419f89bc1e760503e2443897ab11
SalavathSanthosh <53509668+SalavathSanthosh@users.noreply.github.com>
2019-10-30 18:42:45 +0530

    Update SpringBootFirstApp123456UsingSpringDataApplicationTests.java

--- src/test/java/com/nit/demo/SpringBootFirstApp123456UsingSpringDataApplicationTests.java
+++ src/test/java/com/nit/demo/SpringBootFirstApp123456UsingSpringDataApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SpringBootFirstApp123456UsingSpringDataApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() 
 	}
 
 }
